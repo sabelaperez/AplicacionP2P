87e2f983a561283f20c37610b340ce7081c0dff7
Diego <diegopalvarez@gmail.com>
2025-10-21 16:28:56 +0200

    Versión 2, con envío de mensajes y nombres

--- LadoCliente/InterfaceCliente.java
+++ LadoCliente/InterfaceCliente.java
@@ -2,9 +2,11 @@ package LadoCliente;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
+import java.util.Set;
 
 public interface InterfaceCliente extends Remote {
 
     public boolean addUsuarioEnLinea(InterfacePeer usuario) throws RemoteException;
-    public boolean removeUsuarioEnLinea(InterfacePeer usuario) throws RemoteException;
+    public boolean removeUsuarioEnLinea(String nombre) throws RemoteException;
+    public Set<String> getPeerNames() throws RemoteException;
 }
